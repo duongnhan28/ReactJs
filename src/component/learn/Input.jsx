18bb4f999aa58e3264d55bfb9b047566ab71f657
duongnhan28 <duongnhan335@example.com>
2025-05-18 13:29:45 +0700

    test RB

--- src/component/learn/Input.jsx
+++ src/component/learn/Input.jsx
@@ -1,11 +1,23 @@
 const Input = (props) => {
 
     const { addNewTo } = props;
+
+
     //addNewTo("A");
+    const handelClick = () => {
+        alert("click me");
+    }
+
+    const handelOnChange = (name) => {
+        console.log(name);
+    }
+
+    // check rebase
+
     return (
         <div className='todo-input'>
-            <input type="text" placeholder="Input here"></input>
-            <button >Add</button>
+            <input onChange={(event) => handelOnChange(event.target.value)} type="text" placeholder="Input here"></input>
+            <button onClick={handelClick}>Add</button>
 
         </div>
     );

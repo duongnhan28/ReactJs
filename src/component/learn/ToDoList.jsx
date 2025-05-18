031c8bb26a1bab06bed12aa1a597e8297bc13bdd
duongnhan28 <duongnhan335@example.com>
2025-05-18 13:31:43 +0700

    conf

--- src/component/learn/ToDoList.jsx
+++ src/component/learn/ToDoList.jsx
@@ -1,6 +1,6 @@
 const ToDoList = (props) => {
     const { name, age, data } = props;
-
+    //main
     return (
         <div className="todo-list">
             <div className="todo-item">{name}</div>
